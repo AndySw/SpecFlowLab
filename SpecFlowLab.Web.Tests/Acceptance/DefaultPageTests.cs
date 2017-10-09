72d62ec4f120fa4dcf66e75b045546791fa78c8b
Andy Sweetman <Andy.Sweetman@288group.com>
2017-10-09 08:56:53 +0100

    Added default page tests

---
+++ SpecFlowLab.Web.Tests/Acceptance/DefaultPageTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecFlowLab.TestFramework;
+using SpecFlowLab.TestFramework.Pages;
+
+namespace SpecFlowLab.Web.Tests.Acceptance
+{
+    [TestClass]
+    public class DefaultPageTests : AcceptanceTest
+    {
+        [ClassInitialize]
+        public new static void TestFixtureSetup(TestContext testContext) { AcceptanceTest.TestFixtureSetup(testContext); }
+
+        [ClassCleanup]
+        public new static void TestFixtureTearDown() { AcceptanceTest.TestFixtureTearDown(); }
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            Pages.Default.Goto();
+        }
+
+        [TestMethod]
+        public void When_I_click_the_AspNet_link_Then_it_will_load_the_MS_AspNet_website()
+        {
+            // arrange
+            var expected = "https://www.asp.net/";
+            // act
+            Pages.Default.AspNetLink();
+            // assert
+            var actual = Browser.Url;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_I_click_the_GettingStarted_link_Then_it_will_load_the_MS_AspNet_WebForms_page()
+        {
+            // arrange
+            var expected = "https://www.asp.net/web-forms";
+            // act
+            Pages.Default.GettingStartedLink();
+            // assert
+            var actual = Browser.Url;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_I_click_the_GetLibraries_link_Then_it_will_load_the_NuGet_website()
+        {
+            // arrange
+            var expected = "https://www.nuget.org/";
+            // act
+            Pages.Default.GetLibrariesLink();
+            // assert
+            var actual = Browser.Url;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_I_click_the_WebHosting_link_Then_it_will_load_the_MS_AspNet_Identity_page()
+        {
+            // arrange
+            var expected = "https://www.asp.net/identity";
+            // act
+            Pages.Default.WebHostingLink();
+            // assert
+            var actual = Browser.Url;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- SpecFlowLab.Web.Tests/Acceptance/TopNavigationTests.cs
+++ SpecFlowLab.Web.Tests/Acceptance/TopNavigationTests.cs
@@ -16,12 +16,36 @@ namespace SpecFlowLab.Web.Tests.Acceptance
         public new static void TestFixtureTearDown() { AcceptanceTest.TestFixtureTearDown(); }
 
         [TestMethod]
+        public void When_I_click_the_Home_link_Then_it_will_load_the_Default_page()
+        {
+            // arrange
+            var expected = "/";
+            // act
+            Pages.TopNavigation.Home();
+            // assert
+            var actual = new Uri(Browser.Url).PathAndQuery;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void When_I_click_the_About_link_Then_it_will_load_the_About_page()
         {
             // arrange
             var expected = "/About";
             // act
             Pages.TopNavigation.About();
+            // assert
+            var actual = new Uri(Browser.Url).PathAndQuery;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_I_click_the_Contact_link_Then_it_will_load_the_Contact_page()
+        {
+            // arrange
+            var expected = "/Contact";
+            // act
+            Pages.TopNavigation.Contact();
             // assert
             var actual = new Uri(Browser.Url).PathAndQuery;
             Assert.AreEqual(expected, actual);
