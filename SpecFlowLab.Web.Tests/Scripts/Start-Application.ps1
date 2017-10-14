d454f13e9adaf3fd49b099b7f071c4e0bc34fa42
Andy Sweetman <adsweetman@gmail.com>
2017-10-14 22:56:34 +0100

    Work in progress on the script to start iis

--- SpecFlowLab.Web.Tests/Scripts/Start-Application.ps1
+++ SpecFlowLab.Web.Tests/Scripts/Start-Application.ps1
@@ -1,8 +1,14 @@
-$iisExpressExe = '"C:\Program Files (x86)\IIS Express\iisexpress.exe"'
-$path = "C:\Users\andrew.sweetman\Projects\github\SpecFlowLab\.vs\config\applicationhost.config"
-$site = "SpecFlowLab.Web"
-$apppool = "Clr4IntegratedAppPool"
-$params = "/config:$path /site:$site"
-Write-Host Get-Location
+param( 
+    [string] $path = $(throw "Path is required."),
+	[string] $port = $(throw "Port is required.")
+)
 Get-Process | where { $_.ProcessName -like "IISExpress" } | Stop-Process
-Start-Process -FilePath $iisExpressExe -ArgumentList $params
+$iisExpressExe = '`"c:\Program Files (x86)\IIS Express\iisexpress.exe`"'
+$path = Resolve-Path $path
+$params = "/path:`"$path`" /port:$port"
+Write-Host $params
+Write-Host "Press Enter to continue"
+Read-Host
+cmd /c start cmd /k "$iisExpressExe $params"
+Start-Sleep -m 1000
+Write-Host "Site started"
